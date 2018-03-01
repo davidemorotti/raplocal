e50c6571d0d3419adaaab880b2795f69fc87b29c
Satuk Kolan <34@satuk.ch>
2018-03-01 16:03:09 +0100

    added an example
    https://codepen.io/tyrus/pen/BNZbbE

---
+++ highlight-example/main.js
@@ -0,0 +1,30 @@
+$(function(){
+	var menu = $('#highlight_menu');
+
+	$(document.body).on('mouseup', function (evt) {
+		var s = document.getSelection(),
+			r = s.getRangeAt(0);
+		if (r && s.toString()) {
+			var p = r.getBoundingClientRect();
+			if (p.left || p.top) {
+				menu.css({
+					left: (p.left + (p.width / 2)) - (menu.width() / 2),
+					top: (p.top - menu.height() - 10),
+					display: 'block',
+					opacity: 0
+			})
+			.animate({
+				opacity:1
+			}, 300);
+
+			setTimeout(function() {
+				menu.addClass('highlight_menu_animate');
+			}, 10);
+			return;
+		}
+		}
+		menu.animate({ opacity:0 }, function () {
+			menu.hide().removeClass('highlight_menu_animate');
+	});
+	});
+});
