4bea05420f3d1114128af46fa7d51d26a2a936fc
Oleg Lavrovsky <oleg@utou.ch>
2018-03-01 16:15:40 +0100

    Mysql cred

--- superset/superset_config.py
+++ superset/superset_config.py
@@ -11,5 +11,5 @@ CACHE_CONFIG = {
     'CACHE_REDIS_PORT': 6379,
     'CACHE_REDIS_DB': 1,
     'CACHE_REDIS_URL': 'redis://redis:6379/1'}
-SQLALCHEMY_DATABASE_URI = 'mysql://superset:superset@mysql:3306/superset'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:graphcool@mysql:3306/superset'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
