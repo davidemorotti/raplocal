6ae7894c30ab1d14c20cdafc3f2ecc51c7971e77
Satuk Kolan <34@satuk.ch>
2018-03-02 11:04:15 +0100

    added some sample pages

---
+++ highlight-example/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="graph">
+      
+    </div>
+    
+    <div class="top-trends">
+      <h1>TOP CONTROVERSIAL</h1>
+
+    </div>
+
+    <div class="trending">
+      <h1>TRENDING</h1>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue);
+
+export default {
+  head: {
+    title: 'Home page'
+  }
+}
+</script>
